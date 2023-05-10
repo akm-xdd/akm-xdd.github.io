1e5316a2e03a630b9255fe2bc30bde29844f3212
Anand Kumar Mishra <110248822+akm-xdd@users.noreply.github.com>
2023-05-10 14:51:16 +0530

    build files

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+)
